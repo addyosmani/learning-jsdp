e8c862ffad371261321327540de0d50ae69d78a9
Addy Osmani <addyosmani@gmail.com>
2023-04-28 12:56:04 -0700

    Create MessageInput.js

---
+++ ch12/react-loading-patterns/MessageInput.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const MessageInput = ({ onSendMessage }) => {
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (message.trim()) {
+      onSendMessage(message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+export default MessageInput;
