eee6e6d56f490ab46082a337ae953b5f24b031c7
Addy Osmani <addyosmani@gmail.com>
2023-04-28 21:31:27 -0700

    Update ESM gallery implem

--- ch05/gallery/components/gallery.js
+++ ch05/gallery/components/gallery.js
@@ -4,7 +4,14 @@ export async function createGallery() {
   const gallery = document.createElement('div');
   gallery.classList.add('gallery');
 
-  const photos = await fetchDogImages();
+  let photos;
+  try {
+    photos = await fetchDogImages();
+  } catch (error) {
+    console.log('Error fetching dog images:', error);
+    // Fallback to local JSON file
+    photos = await fetchBackupDogImages();
+  }
 
   for (const photo of photos) {
     const photoElement = await createPhoto(photo.url, 'Cute Dog');
@@ -18,4 +25,10 @@ async function fetchDogImages() {
   const response = await fetch('https://dog.ceo/api/breeds/image/random/10');
   const data = await response.json();
   return data.message.map(url => ({ url }));
+}
+
+async function fetchBackupDogImages() {
+  const response = await fetch('gallery/backup-dogs.json');
+  const data = await response.json();
+  return data.map(url => ({ url }));
 }
